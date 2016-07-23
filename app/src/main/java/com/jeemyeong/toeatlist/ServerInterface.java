12c11c0d84c798fa05f923d81ade8b7fe9d7c980
Jeemyeong Lee <ljm901011@gmail.com>
2016-07-23 16:45:36 +0900

    Edit ServerInterface

--- app/src/main/java/com/jeemyeong/toeatlist/ServerInterface.java
+++ app/src/main/java/com/jeemyeong/toeatlist/ServerInterface.java
@@ -26,11 +26,14 @@ public interface ServerInterface {
     public void getFoods(Callback<List<Food>> callback);
 
     /**
-     * 여러 게시글 중에서 하나에 해당하는 게시글을 GET방식으로 요청하는 메소드
+     * Client으로부터 Server에게 POST방식으로 데이터를 전송하는 메소드
      *
-     * /contents/{article-id}에서 article-id 부분에 선택된 게시글의 id가 붙여져서 요청
-     *
-     * @param id
+     * @param name
+     * @param category
+     * @param description
+     * @param restaurant
+     * @param loca_simple
+     * @param image
      * @param callback
      */
 
@@ -42,25 +45,5 @@ public interface ServerInterface {
                        @Part("restaurant")String restaurant,
                        @Part("loca_simple")String loca_simple,
                        @Part("image")TypedFile image,
-
                        Callback<Object> callback);
-
-//    @GET("/contents/{article-id}")
-//    public void getContentById(@Path("article-id") long id, Callback<Content> callback);
-//
-//    /**
-//     * 게시글의 내용을 객체로 보내는 부분
-//     *
-//     * 헤더에 Content Type : application/json으로 자료가 어떤 방식으로 요청 및 응답하는지 표시한다.
-//     * content를 보내고 그에 대한 결과로 Thumbnail을 받는다.
-//     *
-//     * @param content
-//     * @param callback
-//     */
-//
-//    @Headers("Content-Type: application/json")
-//    @POST("/contents")
-//    public void newContent(@Body Content content, Callback<Thumbnail> callback);
-
-
 }
